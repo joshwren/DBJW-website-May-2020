b2e33faefe5ccaf7e78d3a147cb39502fa2a9013
Joshua Wren <joshuawren@Joshuas-iMac.local>
2020-07-31 12:12:28 +1000

    Live commit 2

--- docs/message_sent.php
+++ docs/message_sent.php
@@ -1,4 +1,3 @@
-<!-- ! tab creates a new boiler plate -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,12 +10,89 @@
       <link rel="shortcut icon" type="image/png" href="assets/images/logos/DBJW_logo.png">
     
       <!-- Custom style sheet location -->
-      <link rel="stylesheet" href="assets/styles/message-sent.css">
+      <link rel="stylesheet" href="message-sent.css">
     
       <meta name="keywords" content="graphic design, website design, website development, great graphic design">
       <meta name="description" content="Designs by Josh Wren. Highly Experienced Graphic and Web Designer. Highly Experienced Web Developer.">
-<link href="styles.ffcec5bdae6ea1c1ca1f.css" rel="stylesheet"></head>
+</head>
 <body>
+  <!-- PHP FORM SEND SCRIPT START HERE -->
+
+<?php
+if(isset($_POST['email'])) {
+ 
+    // Email send details along with the subject line
+    $email_to = "joshua@advancedgraphics.com.au";
+    $email_subject = "Email from DBJW Portfolio website";
+ 
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+ 
+ 
+    // validation expected data exists
+    if(!isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['telephone']) ||
+        !isset($_POST['comments'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+ 
+     
+ 
+    $name = $_POST['name']; // required
+    $email_from = $_POST['email']; // required
+    $telephone = $_POST['telephone']; // not required
+    $comments = $_POST['comments']; // required
+ 
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$name)) {
+    $error_message .= 'The Name you entered does not appear to be valid.<br />';
+  }
+ 
+  if(strlen($comments) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+ 
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+ 
+     
+ 
+    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+ 
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+?>
+
 <!-- CONTENT START HERE -->
 
   <div class="message-sent">
@@ -69,5 +145,10 @@
 <script>
 
 </script>
-<script type="text/javascript" src="vendors~main.bc4e00d6cdefeb298605.js"></script><script type="text/javascript" src="main.ffcec5bdae6ea1c1ca1f.js"></script></body>
+
+<?php
+}
+?>
+
+</body>
 </html>
