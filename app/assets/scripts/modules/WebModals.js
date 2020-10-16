58b23e93e39cf439e37122473849d9de2e21c022
Josh <joshuawren@outlook.com>
2020-10-16 22:55:18 +1100

    Website gallery added

---
+++ app/assets/scripts/modules/WebModals.js
@@ -0,0 +1,201 @@
+// ----- Get the LOGOS MODAL 1 ----- //
+var webModal01 = document.getElementById("webModal01");
+var webModal02 = document.getElementById("webModal02");
+var webModal03 = document.getElementById("webModal03");
+var webModal04 = document.getElementById("webModal04");
+var webModal05 = document.getElementById("webModal05");
+var webModal06 = document.getElementById("webModal06");
+var webModal07 = document.getElementById("webModal07");
+var webModal08 = document.getElementById("webModal08");
+var webModal09 = document.getElementById("webModal09");
+var webModal10 = document.getElementById("webModal10");
+var webModal11 = document.getElementById("webModal11");
+var webModal12 = document.getElementById("webModal12");
+var webModal13 = document.getElementById("webModal13");
+var webModal14 = document.getElementById("webModal14");
+var webModal15 = document.getElementById("webModal15");
+var webModal16 = document.getElementById("webModal16");
+var webModal17 = document.getElementById("webModal17");
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var webWorksImg01 = document.getElementById("webWorksImg01");
+var webWorksImg02 = document.getElementById("webWorksImg02");
+var webWorksImg03 = document.getElementById("webWorksImg03");
+var webWorksImg04 = document.getElementById("webWorksImg04");
+var webWorksImg05 = document.getElementById("webWorksImg05");
+var webWorksImg06 = document.getElementById("webWorksImg06");
+var webWorksImg07 = document.getElementById("webWorksImg07");
+var webWorksImg08 = document.getElementById("webWorksImg08");
+var webWorksImg09 = document.getElementById("webWorksImg09");
+var webModalImg10 = document.getElementById
+("webWorksImg10");
+var webModalImg11 = document.getElementById
+("webWorksImg11");
+var webModalImg12 = document.getElementById
+("webWorksImg12");
+var webModalImg13 = document.getElementById
+("webWorksImg13");
+var webModalImg14 = document.getElementById
+("webWorksImg14");
+var webModalImg15 = document.getElementById
+("webWorksImg15");
+var webModalImg16 = document.getElementById
+("webWorksImg16");
+var webModalImg17 = document.getElementById
+("webWorksImg17");
+var webModalImg01 = document.getElementById("webImg01");
+var webModalImg02 = document.getElementById("webImg02");
+var webModalImg03 = document.getElementById("webImg03");
+var webModalImg04 = document.getElementById("webImg04");
+var webModalImg05 = document.getElementById("webImg05");
+var webModalImg06 = document.getElementById("webImg06");
+var webModalImg07 = document.getElementById("webImg07");
+var webModalImg08 = document.getElementById("webImg08");
+var webModalImg09 = document.getElementById("webImg09");
+var webModalImg10 = document.getElementById("webImg10");
+var webModalImg11 = document.getElementById("webImg11");
+var webModalImg12 = document.getElementById("webImg12");
+var webModalImg13 = document.getElementById("webImg13");
+var webModalImg14 = document.getElementById("webImg14");
+var webModalImg15 = document.getElementById("webImg15");
+var webModalImg16 = document.getElementById("webImg16");
+var webModalImg17 = document.getElementById("webImg17");
+// var captionText = document.getElementById("caption");
+webWorksImg01.onclick = function(){
+    webModal01.style.display = "block";
+    webImg01.src = this.src;
+    // captionText.innerHTML = this.alt;
+}
+webWorksImg02.onclick = function(){
+    webModal02.style.display = "block";
+    webImg02.src = this.src;
+}
+webWorksImg03.onclick = function(){
+    webModal03.style.display = "block";
+    webImg03.src = this.src;
+}
+webWorksImg04.onclick = function(){
+    webModal04.style.display = "block";
+    webImg04.src = this.src;
+}
+webWorksImg05.onclick = function(){
+    webModal05.style.display = "block";
+    webImg05.src = this.src;
+}
+webWorksImg06.onclick = function(){
+    webModal06.style.display = "block";
+    webImg06.src = this.src;
+}
+webWorksImg07.onclick = function(){
+    webModal07.style.display = "block";
+    webImg07.src = this.src;
+}
+webWorksImg08.onclick = function(){
+    webModal08.style.display = "block";
+    webImg08.src = this.src;
+}
+webWorksImg09.onclick = function(){
+  webModal09.style.display = "block";
+  webImg09.src = this.src;
+}
+webWorksImg10.onclick = function(){
+  webModal10.style.display = "block";
+  webImg10.src = this.src;
+}
+webWorksImg11.onclick = function(){
+  webModal11.style.display = "block";
+  webImg11.src = this.src;
+}
+webWorksImg12.onclick = function(){
+  webModal12.style.display = "block";
+  webImg12.src = this.src;
+}
+webWorksImg13.onclick = function(){
+  webModal13.style.display = "block";
+  webImg13.src = this.src;
+}
+webWorksImg14.onclick = function(){
+  webModal14.style.display = "block";
+  webImg14.src = this.src;
+}
+webWorksImg15.onclick = function(){
+  webModal15.style.display = "block";
+  webImg15.src = this.src;
+}
+webWorksImg16.onclick = function(){
+  webModal16.style.display = "block";
+  webImg16.src = this.src;
+}
+webWorksImg17.onclick = function(){
+  webModal17.style.display = "block";
+  webImg17.src = this.src;
+}
+// Get the <span> element that closes the modal
+var webModalClose01 = document.getElementsByClassName("webModalClose01")[0];
+var webModalClose02 = document.getElementsByClassName("webModalClose02")[0];
+var webModalClose03 = document.getElementsByClassName("webModalClose03")[0];
+var webModalClose04 = document.getElementsByClassName("webModalClose04")[0];
+var webModalClose05 = document.getElementsByClassName("webModalClose05")[0];
+var webModalClose06 = document.getElementsByClassName("webModalClose06")[0];
+var webModalClose07 = document.getElementsByClassName("webModalClose07")[0];
+var webModalClose08 = document.getElementsByClassName("webModalClose08")[0];
+var webModalClose09 = document.getElementsByClassName("webModalClose09")[0];
+var webModalClose10 = document.getElementsByClassName("webModalClose10")[0];
+var webModalClose11 = document.getElementsByClassName("webModalClose11")[0];
+var webModalClose12 = document.getElementsByClassName("webModalClose12")[0];
+var webModalClose13 = document.getElementsByClassName("webModalClose13")[0];
+var webModalClose14 = document.getElementsByClassName("webModalClose14")[0];
+var webModalClose15 = document.getElementsByClassName("webModalClose15")[0];
+var webModalClose16 = document.getElementsByClassName("webModalClose16")[0];
+var webModalClose17 = document.getElementsByClassName("webModalClose17")[0];
+// When the user clicks on <span> (x), close the modal
+webModalClose01.onclick = function() {
+    webModal01.style.display = "none";
+  }
+webModalClose02.onclick = function() {
+    webModal02.style.display = "none";
+  }
+webModalClose03.onclick = function() {
+    webModal03.style.display = "none";
+  }
+webModalClose04.onclick = function() {
+    webModal04.style.display = "none";
+  }
+webModalClose05.onclick = function() {
+    webModal05.style.display = "none";
+  }
+webModalClose06.onclick = function() {
+    webModal06.style.display = "none";
+  }
+webModalClose07.onclick = function() {
+    webModal07.style.display = "none";
+  }
+webModalClose08.onclick = function() {
+    webModal08.style.display = "none";
+  }
+ webModalClose09.onclick = function() {
+    webModal09.style.display = "none";
+  }
+  webModalClose10.onclick = function() {
+    webModal10.style.display = "none";
+  }
+  webModalClose11.onclick = function() {
+    webModal11.style.display = "none";
+  }
+  webModalClose12.onclick = function() {
+    webModal12.style.display = "none";
+  }
+  webModalClose13.onclick = function() {
+    webModal13.style.display = "none";
+  }
+  webModalClose14.onclick = function() {
+    webModal14.style.display = "none";
+  }
+  webModalClose15.onclick = function() {
+    webModal15.style.display = "none";
+  }
+  webModalClose16.onclick = function() {
+    webModal16.style.display = "none";
+  }
+  webModalClose17.onclick = function() {
+    webModal17.style.display = "none";
+  }
